e4753e90e235c0645d222f09b2a0e597aa6f906b
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-01-10 17:51:04 -0700

    Update chefs controller for update method; Complete chefs show page view; Complete US2

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,5 +1,13 @@
 class ChefsController < ApplicationController
   def show
-    
+    @chef = Chef.find(params[:id])
+  end
+  
+  def update
+    @chef = Chef.find(params[:id])
+    @dish = Dish.find(params[:dish_id])
+
+    @chef.dishes << @dish
+    redirect_to chef_path(@chef)
   end
 end
