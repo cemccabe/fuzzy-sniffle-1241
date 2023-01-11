4976db744626d570fd4dddde6964abc28f9e755f
cemccabe <46434166+cemccabe@users.noreply.github.com>
2023-01-10 17:16:54 -0700

    Create tests for chefs show page

---
+++ spec/features/chefs/show_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe 'Chef show page' do
+  before(:each) do
+    @gordon = Chef.create!(name: 'Gordon Ramsay')
+    @sushi = @gordon.dishes.create!(name: 'Sushi', description: 'Japanese dish')
+    @salad = @gordon.dishes.create!(name: 'Salad', description: 'Healthy appetizer')
+    @rice = Ingredient.create!(name: 'Rice', calories: 200)
+    @salmon = Ingredient.create!(name: 'Salmon', calories: 150)
+    @lettuce = Ingredient.create!(name: 'Lettuce', calories: 30)
+    @dressing = Ingredient.create!(name: 'Dressing', calories: 250)
+    DishIngredient.create!(dish_id: @sushi.id, ingredient_id: @rice.id)
+    DishIngredient.create!(dish_id: @sushi.id, ingredient_id: @salmon.id)
+    DishIngredient.create!(dish_id: @salad.id, ingredient_id: @lettuce.id)
+    DishIngredient.create!(dish_id: @salad.id, ingredient_id: @dressing.id)
+
+    @emeril = Chef.create!(name: 'Emeril Lagasse')
+
+    @steak = Dish.create!(name: 'Steak and Potatoes', description: 'Classic meal', chef_id: @emeril.id)
+    @beef = Ingredient.create!(name: 'Beef', calories: 500)
+    @potatoes = Ingredient.create!(name: 'Potatoes', calories: 425)
+    DishIngredient.create!(dish_id: @steak.id, ingredient_id: @beef.id)
+    DishIngredient.create!(dish_id: @steak.id, ingredient_id: @potatoes.id)
+
+    # @sushi = Dish.create!(name: 'Sushi', description: 'Japanese dish', chef_id: @gordon.id)
+    # @salad = Dish.create!(name: 'Salad', description: 'Healthy appetizer', chef_id: @gordon.id)
+    # @rice = Ingredient.create!(name: 'Rice', calories: 200)
+    # @salmon = Ingredient.create!(name: 'Salmon', calories: 150)
+    # @lettuce = Ingredient.create!(name: 'Lettuce', calories: 30)
+    # @dressing = Ingredient.create!(name: 'Dressing', calories: 250)
+    # DishIngredient.create!(dish_id: @sushi.id, ingredient_id: @rice.id)
+    # DishIngredient.create!(dish_id: @sushi.id, ingredient_id: @salmon.id)
+    # DishIngredient.create!(dish_id: @salad.id, ingredient_id: @lettuce.id)
+    # DishIngredient.create!(dish_id: @salad.id, ingredient_id: @dressing.id)
+
+    visit chef_path(@gordon)
+  end
+
+  describe 'When I visit a chef\'s show page' do
+    it 'I see the name of that chef' do
+      expect(page).to have_content(@gordon.name)
+    end
+
+    it 'and I see a list of all dishes that belong to that chef' do
+      expect(page).to have_content(@sushi.name)
+      expect(page).to have_content(@salad.name)
+    end
+
+    it 'and I see a form to add an existing dish to that chef' do
+      expect(page).to have_field('dish_id')
+    end
+
+    it 'When I fill in the form with the ID of a dish that exists in the database And I click Submit' do
+      fill_in('dish_id', with: "#{@salad.id}")
+      click_button('Submit')
+      expect(current_path).to eq(chef_path(@gordon))
+      expect(page).to have_content(@salad.name)
+    end
+  end
+
+
+
+end
